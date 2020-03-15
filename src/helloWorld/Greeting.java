9eaf2acb78162e2a85ae615063e4f9f936621c51
amin3696 <amin3696@yahoo.com>
2020-03-15 15:28:01 +0100

    modularity remove free spaces

--- src/helloWorld/Greeting.java
+++ src/helloWorld/Greeting.java
@@ -1,12 +1,13 @@
 package helloWorld;
 
+
 import java.awt.image.BufferedImage;
 
 /**
  * @author AminSh
  */
 public class Greeting {
-// all I need to make this modular , is add module info file
+    // all I need to make this modular , is add module info file
     // this is the file that will describe my module and how it relates to other module
     public static void main(String[] args) {
 
--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,7 @@
 //there are 2 thing that I can do in this file
 //1.specify if I want to use any other modules, to do this I use "requires" (line 8)in the body and then the name of the module
-
 //2.the second thing that I can do in this file is : export package from my module , at the moment nobody can use my
 // Greeting class, but if I add "export" (line 9) this will make helloWorld package available to the hole world
-
-
 // this is real modularization , choosing what can and what can not be seen by the outside world.
 
 module AdvancedJavaProgrammingII {
