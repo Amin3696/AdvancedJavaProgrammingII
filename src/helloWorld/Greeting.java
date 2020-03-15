d94da95005dbba88ad018a5739a760aa3645f6ec
Shahimian <shahimmo@at-work.local>
2020-03-15 14:58:06 +0100

    chapter 5 Modular Programming in JAVA JDK-13.0.2

---
+++ src/helloWorld/Greeting.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * @author AminSh
+ */
+public class Greeting {
+// all I need to make this modular , is add module info file
+    // this is the file that will describe my module and how it relates to other module
+    public static void main(String[] args) {
+
+        BufferedImage image;
+        System.out.println("Hello World!");
+    }
+
+}
